eaa03fb1b91271605266d3ae1616a01d85ea8df7
Ritik Kumar <98963769+krritik01@users.noreply.github.com>
2023-02-04 17:50:12 +0530

    Add files via upload

---
+++ tut1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+int main()
+{
+   std::cout << "Hello world";
+   return 0;
+}
+
+/* this is how to write a multi line comment
+
+#inclide <bits/stdc++.h>
+   using namespace std;
+   int main()
+   (
+      cout<<"Hello world";
+      return 0;
+   )
+    */
+   
+/*
+
+#include<iostream>
+using namespace std;
+// This is another way to write
+int main(){
+
+ cout  "<<" this is called insertion operator
+   cout<<"Helo rItik";
+   return 0;
+}
+
+*/
