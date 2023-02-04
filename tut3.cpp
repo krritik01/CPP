d414f6e6dc81ad4c26d280ae64ac04d97db6c10f
Ritik Kumar <98963769+krritik01@users.noreply.github.com>
2023-02-04 17:51:01 +0530

    Add files via upload

---
+++ tut3.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int ravi_has_money;
+    cout << "ravi_has_money\n";
+    cin >> ravi_has_money;
+
+    if (ravi_has_money > 20 && ravi_has_money < 30)
+    {0
+        cout << "Give it to Ritik\n";
+    }
+    else if (ravi_has_money > 30 && ravi_has_money < 50)
+    {
+        cout << "Give it to Rajeshwar\n";
+    }
+    else
+        {
+            cout << "Keep with himself";
+        }
+    return 0;
+}
