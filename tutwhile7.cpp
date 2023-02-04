a116a0dd5f61e872ab1f9fe8eee8882daab8fbcb
Ritik Kumar <98963769+krritik01@users.noreply.github.com>
2023-02-04 17:52:09 +0530

    Add files via upload

---
+++ tutwhile7.cpp
@@ -0,0 +1,15 @@
+// print only positive number
+
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    
+int n;
+cout<<"Enter the number\n";
+cin>>n;
+    while(n>0){
+        cout<<"Number is "<<n;
+        cin>>n;
+    }
+return 0;
+}
