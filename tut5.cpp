611abf602da1cf1a0db75d16ca0f299bebe978f9
Ritik Kumar <98963769+krritik01@users.noreply.github.com>
2023-02-04 17:51:33 +0530

    Add files via upload

---
+++ tut5.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    int n;
+    cout << "Enter the number\n";
+    cin >> n;
+
+    // This is (%) modulo operator
+
+    if (n % 2 == 0)
+    {
+        cout << "Number is even" << endl;
+    }
+    else
+    {
+        cout << "Number is odd" << endl;
+    }
+}
