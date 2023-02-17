95b81fc76472469cbae26b01d093faa13bd0aaac
Ritik Kumar <98963769+krritik01@users.noreply.github.com>
2023-02-18 01:02:29 +0530

    Add files via upload

---
+++ Table.cpp
@@ -0,0 +1,17 @@
+// Print table of any number
+
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    
+int n;
+cin>>n;
+
+
+for (int i = 1; i <= 10; i++)
+{
+    cout<<n*i<<endl;
+}
+
+return 0;
+}
