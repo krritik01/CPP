8abd12cc27198193c94e412d05ba8c5702bef9d9
Ritik Kumar <98963769+krritik01@users.noreply.github.com>
2023-02-19 00:53:51 +0530

    Add files via upload

---
+++ pattern16.cpp
@@ -0,0 +1,23 @@
+/*code to print
+A B C
+B C D
+C D E
+*/
+#include<iostream>
+using namespace std;
+int main()
+{
+int n;
+cin>>n;
+for(int i=1; i<=n; i++)
+{
+for(int j=1; j<=n; j++)
+{ 
+char ch = 'A'+i+j-2;
+cout<<ch<<" ";
+ch++;
+}
+cout<<endl;
+}
+return 0;
+}
