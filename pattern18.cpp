5f79bb9e3e5a856e643e0b41991b1d1c72669092
Ritik Kumar <98963769+krritik01@users.noreply.github.com>
2023-02-20 16:41:24 +0530

    Add files via upload

---
+++ pattern18.cpp
@@ -0,0 +1,23 @@
+/*code to print
+A
+A B
+A B C
+A B C D
+*/
+#include <iostream>
+using namespace std;
+int main()
+{
+ int n;
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        char ch = 'A';
+        for (int j = 1; j <= i; j++)
+        {
+            cout << ch << " ";
+            ch++;
+        }
+        cout << endl;
+    }
+}
