da58f2c3ab87ccd203307564737d0b735ed081b5
Ritik Kumar <98963769+krritik01@users.noreply.github.com>
2023-02-21 00:10:25 +0530

    Add files via upload

---
+++ pattern20.cpp
@@ -0,0 +1,23 @@
+/*code to print
+D
+C D
+B C D
+A B C D
+*/
+#include <bits/stdc++.h>
+using namespace std;
+int main(){     
+int n;
+cin>>n;
+for (int i = 1; i <= n; i++)
+{
+        char start='A'+n-i;
+        for (int j = 1; j <=i; j++)
+        {                
+              cout<<start;
+              start++; 
+        }
+        cout<<endl;       
+}  
+return 0;
+}
